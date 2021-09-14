be42093f4b85d8388699f849f59669bda33e897c
elevu <e.m.container@gmail.com>
2021-09-14 11:56:23 +0200

    Fix samples table

--- src/components/SamplesTable/SamplesTable.tsx
+++ src/components/SamplesTable/SamplesTable.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Input, Table } from 'antd';
-import { sortDate } from '../../services/helpers/helpers';
 import { Batch } from '../../services/interfaces';
 import { Link } from 'react-router-dom';
 
@@ -28,23 +27,104 @@ export const SamplesTable = ({ samples }: SamplesProps) => {
 
   const columns: any = [
     {
-      title: 'Batch ID',
-      dataIndex: 'SampleProject',
-      key: 'SampleProject',
-      render: (SampleProject: any) => (
-        <Link to={`/samples/${SampleProject}`}>{SampleProject}</Link>
+      title: 'Sample name',
+      dataIndex: 'SampleID',
+      key: 'SampleID',
+      fixed: 'left',
+      render: (SampleID: any) => (
+        <Link to={`/samples/${SampleID}`}>{SampleID}</Link>
       ),
     },
     {
-      title: 'Sequencing Date',
-      dataIndex: 'SequencingDate',
-      key: 'SequencingDate',
-      sorter: (a, b) => sortDate(a.SequencingDate, b.SequencingDate),
+      title: 'Batch ID',
+      dataIndex: 'SampleProject',
+      key: 'SampleProject',
+      fixed: 'left',
+      render: (SampleProject: any) => (
+        <Link to={`/batches/${SampleProject}`}>{SampleProject}</Link>
+      ),
     },
     {
-      title: 'Flowcell ID',
-      dataIndex: 'Flowcell',
-      key: 'Flowcell',
+      title: 'Zscore 13',
+      dataIndex: 'Zscore_13',
+      key: 'Zscore_13',
+    },
+    {
+      title: 'Zscore 18',
+      dataIndex: 'Zscore_18',
+      key: 'Zscore_18',
+    },
+    {
+      title: 'Zscore 21',
+      dataIndex: 'Zscore_21',
+      key: 'Zscore_21',
+    },
+    {
+      title: 'Zscore X',
+      dataIndex: 'Zscore_X',
+      key: 'Zscore_X',
+    },
+    {
+      title: 'FF-PF (%)',
+      dataIndex: 'FetalFractionPreface',
+      key: 'FetalFractionPreface',
+    },
+    {
+      title: 'FF-X (%)',
+      dataIndex: 'FFX',
+      key: 'FFX',
+    },
+    {
+      title: 'FF-Y (%)',
+      dataIndex: 'FFY',
+      key: 'FFY',
+    },
+    {
+      title: 'Sex',
+      dataIndex: 'sex',
+      key: 'sex',
+    },
+    {
+      title: 'CNV Segment',
+      dataIndex: 'CNVSegment',
+      key: 'CNVSegment',
+    },
+    {
+      title: 'Warning',
+      dataIndex: 'text_warning',
+      key: 'text_warning',
+    },
+    {
+      title: 'QC Flag',
+      dataIndex: 'QCFlag',
+      key: 'QCFlag',
+      width: 150,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Download',
+      dataIndex: '',
+      key: 'segmental_calls',
+    },
+    {
+      title: 'Include',
+      dataIndex: 'include',
+      key: 'include',
+    },
+    {
+      title: 'Comment',
+      dataIndex: 'comment',
+      key: 'comment',
+    },
+    {
+      title: 'Last changed',
+      dataIndex: 'change_include_date',
+      key: 'change_include_date',
+      fixed: 'right',
     },
   ];
 
@@ -59,6 +139,7 @@ export const SamplesTable = ({ samples }: SamplesProps) => {
         columns={columns}
         dataSource={filteredSamples}
         rowKey="SampleProject"
+        scroll={{ x: 1500 }}
       />
     </>
   );
