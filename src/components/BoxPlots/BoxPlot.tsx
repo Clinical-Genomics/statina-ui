ab6f8e21f6eec3d395c45de1a894dd292243a7d2
elevu <e.m.container@gmail.com>
2021-09-28 11:13:11 +0200

    Remove plotly

--- src/components/BoxPlots/BoxPlot.tsx
+++ src/components/BoxPlots/BoxPlot.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Plot from 'react-plotly.js';
 import { TracePlot } from '../../services/interfaces';
 
 type PlotProps = {
@@ -7,19 +6,5 @@ type PlotProps = {
 };
 
 export function BoxPlot(trace: PlotProps) {
-  return (
-    <div>
-      <Plot
-        data={[trace.trace]}
-        layout={{
-          width: 1000,
-          height: 500,
-          yaxis: {
-            title: 'Chr13_Ratio',
-          },
-        }}
-      />
-      ;
-    </div>
-  );
+  return <div></div>;
 }
