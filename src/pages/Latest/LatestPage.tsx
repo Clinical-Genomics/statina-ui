afeeaa422e022798172bc3b743cc2e76908b6916
elevu <e.m.container@gmail.com>
2021-10-22 14:45:20 +0200

    Remove markdown and CHANGELOG

--- src/pages/Latest/LatestPage.tsx
+++ src/pages/Latest/LatestPage.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import raw from 'raw.macro'
-const changelogMarkdown = raw('../../../CHANGELOG.md')
 
 export const LatestPage = () => {
-  return <ReactMarkdown>{changelogMarkdown}</ReactMarkdown>
+  return <div>Updates here</div>
 }
