e68f6fc0512d22b742fff4848d03665e96b6f6fb
elevu <e.m.container@gmail.com>
2021-10-25 15:07:19 +0200

    Add modal

---
+++ src/components/RegisterModal/RegisterModal.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'antd'
+
+export const RegisterModal = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+  return (
+    <>
+      <Button onClick={showModal}>Create new account</Button>
+      <Modal
+        title="Create new account"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  )
+}
