8c45db3cba9e7abb5fbba4bfd536d4c65c1dd9a0
elevu <e.m.container@gmail.com>
2021-11-01 13:50:27 +0100

    Refactor methods

--- src/components/ZscoreGraph/ZscoreGraph.tsx
+++ src/components/ZscoreGraph/ZscoreGraph.tsx
@@ -8,48 +8,41 @@ type ZscoreGraphProps = {
   chromosome: number
 }
 
-export const ZscoreGraph = ({ batchId, chromosome }: ZscoreGraphProps) => {
-  const userContext = useContext(UserContext)
-  const [graph, setGraph] = useState<any>()
-
-  useEffect(() => {
-    getZScoreGraph(batchId, chromosome, userContext).then((response) => {
-      setGraph(response)
-      console.log(response)
-    })
-  }, [])
-  const data: any[] = [
+const buildData = (response, chromosome) => {
+  const data = [
     {
-      name: `Current batch ${graph?.ncv_chrom_data[chromosome].count}`,
-      y: graph?.ncv_chrom_data[chromosome].ncv_values,
-      x: graph?.ncv_chrom_data[chromosome].names,
+      name: `Current batch ${response?.ncv_chrom_data[chromosome].count}`,
+      y: response?.ncv_chrom_data[chromosome].ncv_values,
+      x: response?.ncv_chrom_data[chromosome].names,
       mode: 'markers',
       type: 'scatter',
     },
     {
-      y: graph?.normal_data[chromosome].ncv_values,
-      text: graph?.normal_data[chromosome].names,
+      y: response?.normal_data[chromosome].ncv_values,
+      text: response?.normal_data[chromosome].names,
       pointpos: 30,
       type: 'box',
       marker: {
         color: '#ccccb3',
       },
-      name: `Negative ${graph?.ncv_chrom_data[chromosome].count}`,
+      name: `Negative ${response?.ncv_chrom_data[chromosome].count}`,
     },
   ]
-
-  Object.keys(graph.abnormal_data[chromosome]).forEach((status) => {
+  Object.keys(response.abnormal_data[chromosome]).forEach((status) => {
     data.push({
-      name: `${status} T${chromosome} ${graph.abnormal_data[chromosome][status].count}`,
-      y: graph.abnormal_data[chromosome][status].ncv_values,
-      x: graph.abnormal_data[chromosome][status].x_axis,
-      text: graph.abnormal_data[chromosome][status].names,
+      name: `${status} T${chromosome} ${response.abnormal_data[chromosome][status]?.count}`,
+      y: response.abnormal_data[chromosome][status]?.ncv_values,
+      x: response.abnormal_data[chromosome][status]?.x_axis,
+      text: response.abnormal_data[chromosome][status]?.names,
       mode: 'markers',
       type: 'scatter',
     })
   })
+  return data
+}
 
-  const layout = {
+const buildLayout = (response, chromosome) => {
+  return {
     legend: { hovermode: 'closest' },
     hovermode: 'closest',
     annotations: [],
@@ -59,10 +52,24 @@ export const ZscoreGraph = ({ batchId, chromosome }: ZscoreGraphProps) => {
     },
     yaxis: {
       range: [-10, 10],
-      title: graph?.ncv_chrom_data[chromosome].ncv_values,
+      title: response?.ncv_chrom_data[chromosome].ncv_values,
     },
     width: 1200,
     height: 600,
   }
+}
+
+export const ZscoreGraph = ({ batchId, chromosome }: ZscoreGraphProps) => {
+  const userContext = useContext(UserContext)
+  const [data, setData] = useState<any>()
+  const [layout, setLayout] = useState<any>()
+
+  useEffect(() => {
+    getZScoreGraph(batchId, chromosome, userContext).then((response) => {
+      setData(buildData(response, chromosome))
+      setLayout(buildLayout(response, chromosome))
+    })
+  }, [])
+
   return <Plot data={data} layout={layout} />
 }
