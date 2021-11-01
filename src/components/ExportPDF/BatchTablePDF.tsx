218f154e8b6abb8e1f3b11d10b7f32b40428ba91
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2021-11-01 11:31:45 +0100

    remove unused code

--- src/components/ExportPDF/BatchTablePDF.tsx
+++ src/components/ExportPDF/BatchTablePDF.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import jsPDF from 'jspdf'
 import 'jspdf-autotable'
 import { Tooltip } from 'antd'
