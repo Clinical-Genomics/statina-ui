02eac2367544ff1e8f9b8cc45a3d6a7fdabaaaa1
elevu <e.m.container@gmail.com>
2021-11-11 15:37:26 +0100

    Add status table

--- src/pages/Sample/SamplePage.tsx
+++ src/pages/Sample/SamplePage.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Card, Space, Typography, Tabs, Input } from 'antd'
+import { Card, Space, Typography, Tabs, Input, Table, Select } from 'antd'
 import { SampleInfoBox } from 'components/SampleInfoBox/SampleInfoBox'
-import { SampleStatusTable } from 'components/SampleStatusTable/SampleStatusTable'
 import { editSample, getSample } from '../../services/StatinaApi'
 import { useLocation } from 'react-router-dom'
 import { UserContext } from '../../services/userContext'
@@ -9,6 +8,7 @@ import { Loading } from '../../components/Loading'
 import { ErrorPage } from '../Error/ErrorPage'
 import { SuccessNotification } from '../../services/helpers/helpers'
 import { Sample } from '../../services/interfaces'
+import { sampleStatusTags } from '../../services/helpers/constants'
 
 const { Title, Text } = Typography
 const { TabPane } = Tabs
@@ -18,23 +18,67 @@ export function SamplePage() {
   const [sample, setSample] = useState<Sample>()
   const [error, setError] = useState<any>()
   const userContext = useContext(UserContext)
+  const { Option } = Select
   const { pathname } = useLocation()
   const sampleId = pathname.substring(pathname.lastIndexOf('/') + 1, pathname.length)
 
   useEffect(() => {
-    if (sampleId)
-      getSample(sampleId, userContext)
-        .then((sampleResponse) => {
-          setSample(sampleResponse)
-          setError(null)
-        })
-        .catch((error) => {
-          setError(error)
-        })
+    if (sampleId) retrieveSample()
   }, [sampleId])
 
-  const onChange = (e) => {
-    editSample(sampleId, `comment=${e?.target?.value}`, 'comment', userContext).then((response) => {
+  const retrieveSample = () => {
+    getSample(sampleId, userContext)
+      .then((sampleResponse) => {
+        setSample(sampleResponse)
+        setError(null)
+      })
+      .catch((error) => {
+        setError(error)
+      })
+  }
+
+  const columns = [
+    {
+      title: 'Chromosome abnormality',
+      dataIndex: 'chromosome',
+      key: 'chromosome',
+      render: (key, chromosome) => <div>{chromosome[0]}</div>,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      render: (key, chromosome) => (
+        <Select
+          defaultValue={chromosome[1].status || sampleStatusTags.normal.label}
+          style={{ width: 120 }}
+          onChange={(value) => onStatusChange(value, chromosome[0])}
+        >
+          {Object.values(sampleStatusTags).map((status) => (
+            <Option value={status.label} key={status.label}>
+              {status.label}
+            </Option>
+          ))}
+        </Select>
+      ),
+    },
+    {
+      title: 'Latest change',
+      dataIndex: 'latestChange',
+      key: 'latestChange',
+      render: (key, chromosome) => <div>{chromosome[1].edited}</div>,
+    },
+  ]
+
+  const onStatusChange = (value, chromosome) => {
+    console.log(value)
+    editSample(sampleId, `status=${value}`, `status/${chromosome}?status=${value}`, userContext)
+      .then(() => retrieveSample())
+      .catch(() => setError(true))
+  }
+
+  const onCommentChange = (e) => {
+    editSample(sampleId, `comment=${e?.target?.value}`, 'comment', userContext).then(() => {
       SuccessNotification({
         type: 'success',
         message: 'Comment updated',
@@ -54,12 +98,20 @@ export function SamplePage() {
                   <Text italic type="secondary">
                     Press enter to save
                   </Text>
-                  <TextArea rows={4} onPressEnter={onChange} defaultValue={sample.comment} />
+                  <TextArea rows={4} onPressEnter={onCommentChange} defaultValue={sample.comment} />
                 </Card>
                 <Card>
                   <Tabs defaultActiveKey="1" type="card">
                     <TabPane tab="Status Table" key="1">
-                      Sample status coming soon
+                      <Space size={'large'} direction="vertical" style={{ width: '100%' }}>
+                        <Table
+                          columns={columns}
+                          dataSource={Object.entries(sample.status)}
+                          rowKey="0"
+                          pagination={false}
+                          bordered={true}
+                        />
+                      </Space>
                     </TabPane>
                     <TabPane tab="Tab 2" key="2">
                       test
