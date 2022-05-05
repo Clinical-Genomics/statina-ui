646722257a4ba67e6dd9f1f837dc9a613513f757
Eleonora <eleonora.maccacaro@scilifelab.se>
2022-05-05 13:56:36 +0200

    Fix table pagination

--- src/components/Datasets/Datasets.tsx
+++ src/components/Datasets/Datasets.tsx
@@ -48,94 +48,86 @@ export const Datasets = () => {
       fixed: 'left',
       sorter: (a, b) => a.name.length - b.name.length,
       render: (name: any) => <Link to={`/${name}`}>{name}</Link>,
-      width: 150,
     },
     {
       title: <Tooltip title="Fetal fraction X0">FFX0</Tooltip>,
       dataIndex: 'fetal_fraction_X0',
       key: 'fetal_fraction_X0',
       sorter: (a, b) => a.fetal_fraction_X0 - b.fetal_fraction_X0,
-      width: 70,
     },
     {
       title: <Tooltip title="Fetal fraction XXX">FFXXX</Tooltip>,
       dataIndex: 'fetal_fraction_XXX',
       key: 'fetal_fraction_XXX',
       sorter: (a, b) => a.fetal_fraction_XXX - b.fetal_fraction_XXX,
-      width: 70,
     },
     {
       title: <Tooltip title="Fetal fraction preface">FFPR</Tooltip>,
       dataIndex: 'fetal_fraction_preface',
       key: 'fetal_fraction_preface',
       sorter: (a, b) => a.fetal_fraction_preface - b.fetal_fraction_preface,
-      width: 70,
     },
     {
-      title: <Tooltip title="Fetal fraction y for trisomy">FFY for trisomy</Tooltip>,
+      title: <Tooltip title="Fetal fraction y for trisomy">FFY tris</Tooltip>,
       dataIndex: 'fetal_fraction_y_for_trisomy',
       key: 'fetal_fraction_y_for_trisomy',
       sorter: (a, b) => a.fetal_fraction_y_for_trisomy - b.fetal_fraction_y_for_trisomy,
-      width: 140,
     },
     {
       title: <Tooltip title="Fetal fraction y max">FFY max</Tooltip>,
       dataIndex: 'fetal_fraction_y_max',
       key: 'fetal_fraction_y_max',
       sorter: (a, b) => a.fetal_fraction_y_max - b.fetal_fraction_y_max,
-      width: 100,
     },
     {
       title: <Tooltip title="Fetal fraction y min">FFY min</Tooltip>,
       dataIndex: 'fetal_fraction_y_min',
       key: 'fetal_fraction_y_min',
       sorter: (a, b) => a.fetal_fraction_y_min - b.fetal_fraction_y_min,
-      width: 90,
     },
     {
-      title: 'K lower',
+      title: 'K low',
       dataIndex: 'k_lower',
       key: 'k_lower',
       sorter: (a, b) => a.k_lower - b.k_lower,
-      width: 90,
     },
     {
-      title: 'K upper',
+      title: 'K up',
       dataIndex: 'k_upper',
       key: 'k_upper',
       sorter: (a, b) => a.k_upper - b.k_upper,
       width: 90,
     },
     {
-      title: 'M lower',
+      title: 'M low',
       dataIndex: 'm_lower',
       key: 'm_lower',
       sorter: (a, b) => a.m_lower - b.m_lower,
       width: 90,
     },
     {
-      title: 'M upper',
+      title: 'M up',
       dataIndex: 'm_upper',
       key: 'm_upper',
       sorter: (a, b) => a.m_upper - b.m_upper,
       width: 90,
     },
     {
-      title: 'Trisomy hard max',
+      title: 'Tris hard max',
       dataIndex: 'trisomy_hard_max',
       key: 'trisomy_hard_max',
       sorter: (a, b) => a.trisomy_hard_max - b.trisomy_hard_max,
       width: 150,
     },
     {
-      title: 'Trisomy hard min',
+      title: 'Tris hard min',
       dataIndex: 'trisomy_hard_min',
       key: 'trisomy_hard_min',
       sorter: (a, b) => a.trisomy_hard_min - b.trisomy_hard_min,
       width: 150,
     },
     {
-      title: 'Trisomy soft max',
+      title: 'Tris soft max',
       dataIndex: 'trisomy_soft_max',
       key: 'trisomy_soft_max',
       sorter: (a, b) => a.trisomy_soft_max - b.trisomy_soft_max,
@@ -182,7 +174,6 @@ export const Datasets = () => {
             rowKey={(record: any) => record.name}
             size="small"
             bordered
-            scroll={{ x: 2000 }}
             pagination={false}
             showSorterTooltip={false}
           />
