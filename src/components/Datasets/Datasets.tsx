399f53c5d8136e1377421ae5e1c7ae835f8b0bde
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-05-05 10:45:21 +0200

    Edit tables titles

--- src/components/Datasets/Datasets.tsx
+++ src/components/Datasets/Datasets.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../services/userContext'
 import { getDatasets } from 'services/StatinaApi'
 import { Link } from 'react-router-dom'
-import { Input, Table, Typography } from 'antd'
+import { Input, Table, Tooltip, Typography } from 'antd'
 import { escapeRegExp } from 'services/helpers/helpers'
 import { ErrorPage } from 'pages/Error/ErrorPage'
 import { Loading } from 'components/Loading'
@@ -48,99 +48,115 @@ export const Datasets = () => {
       fixed: 'left',
       sorter: (a, b) => a.name.length - b.name.length,
       render: (name: any) => <Link to={`/${name}`}>{name}</Link>,
+      width: 150,
     },
     {
-      title: 'fetal_fraction_X0',
+      title: <Tooltip title="Fetal fraction X0">FFX0</Tooltip>,
       dataIndex: 'fetal_fraction_X0',
       key: 'fetal_fraction_X0',
       sorter: (a, b) => a.fetal_fraction_X0 - b.fetal_fraction_X0,
+      width: 70,
     },
     {
-      title: 'fetal_fraction_XXX',
+      title: <Tooltip title="Fetal fraction XXX">FFXXX</Tooltip>,
       dataIndex: 'fetal_fraction_XXX',
       key: 'fetal_fraction_XXX',
       sorter: (a, b) => a.fetal_fraction_XXX - b.fetal_fraction_XXX,
+      width: 70,
     },
     {
-      title: 'fetal_fraction_preface',
+      title: <Tooltip title="Fetal fraction preface">FFPR</Tooltip>,
       dataIndex: 'fetal_fraction_preface',
       key: 'fetal_fraction_preface',
       sorter: (a, b) => a.fetal_fraction_preface - b.fetal_fraction_preface,
+      width: 70,
     },
     {
-      title: 'fetal_fraction_y_for_trisomy',
+      title: <Tooltip title="Fetal fraction y for trisomy">FFY_for_trisomy</Tooltip>,
       dataIndex: 'fetal_fraction_y_for_trisomy',
       key: 'fetal_fraction_y_for_trisomy',
       sorter: (a, b) => a.fetal_fraction_y_for_trisomy - b.fetal_fraction_y_for_trisomy,
+      width: 140,
     },
     {
-      title: 'fetal_fraction_y_max',
+      title: <Tooltip title="Fetal fraction y max">FFY_max</Tooltip>,
       dataIndex: 'fetal_fraction_y_max',
       key: 'fetal_fraction_y_max',
       sorter: (a, b) => a.fetal_fraction_y_max - b.fetal_fraction_y_max,
+      width: 100,
     },
     {
-      title: 'fetal_fraction_y_min',
+      title: <Tooltip title="Fetal fraction y min">FFY_min</Tooltip>,
       dataIndex: 'fetal_fraction_y_min',
       key: 'fetal_fraction_y_min',
       sorter: (a, b) => a.fetal_fraction_y_min - b.fetal_fraction_y_min,
+      width: 90,
     },
     {
-      title: 'k_lower',
+      title: 'K_lower',
       dataIndex: 'k_lower',
       key: 'k_lower',
       sorter: (a, b) => a.k_lower - b.k_lower,
+      width: 90,
     },
     {
-      title: 'k_upper',
+      title: 'K_upper',
       dataIndex: 'k_upper',
       key: 'k_upper',
       sorter: (a, b) => a.k_upper - b.k_upper,
+      width: 90,
     },
     {
-      title: 'm_lower',
+      title: 'M_lower',
       dataIndex: 'm_lower',
       key: 'm_lower',
       sorter: (a, b) => a.m_lower - b.m_lower,
+      width: 90,
     },
     {
-      title: 'm_upper',
+      title: 'M_upper',
       dataIndex: 'm_upper',
       key: 'm_upper',
       sorter: (a, b) => a.m_upper - b.m_upper,
+      width: 90,
     },
     {
-      title: 'trisomy_hard_max',
+      title: 'Trisomy_hard_max',
       dataIndex: 'trisomy_hard_max',
       key: 'trisomy_hard_max',
       sorter: (a, b) => a.trisomy_hard_max - b.trisomy_hard_max,
+      width: 150,
     },
     {
-      title: 'trisomy_hard_min',
+      title: 'Trisomy_hard_min',
       dataIndex: 'trisomy_hard_min',
       key: 'trisomy_hard_min',
       sorter: (a, b) => a.trisomy_hard_min - b.trisomy_hard_min,
+      width: 150,
     },
     {
-      title: 'trisomy_soft_max',
+      title: 'Trisomy_soft_max',
       dataIndex: 'trisomy_soft_max',
       key: 'trisomy_soft_max',
       sorter: (a, b) => a.trisomy_soft_max - b.trisomy_soft_max,
+      width: 150,
     },
     {
-      title: 'y_axis_max',
+      title: 'Y_axis_max',
       dataIndex: 'y_axis_max',
       key: 'y_axis_max',
       sorter: (a, b) => a.y_axis_max - b.y_axis_max,
+      width: 110,
     },
     {
-      title: 'y_axis_min',
+      title: 'Y_axis_min',
       dataIndex: 'y_axis_min',
       key: 'y_axis_min',
       sorter: (a, b) => a.y_axis_min - b.y_axis_min,
+      width: 110,
     },
     {
-      title: 'comment',
+      title: 'Comment',
       dataIndex: 'comment',
       key: 'comment',
     },
@@ -166,8 +182,9 @@ export const Datasets = () => {
             rowKey={(record: any) => record.name}
             size="small"
             bordered
-            scroll={{ x: 3600 }}
+            scroll={{ x: 2000 }}
             pagination={false}
+            showSorterTooltip={false}
           />
         </>
       )}
