ca9d0576391151bb78de731107001bd376219145
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-05-13 11:22:45 +0200

    Clean setDataset

--- src/components/DatasetPage/DatasetPage.tsx
+++ src/components/DatasetPage/DatasetPage.tsx
@@ -5,31 +5,13 @@ import { useLocation } from 'react-router-dom'
 import { editDataset, getDataset } from 'services/StatinaApi'
 import { UserContext } from 'services/userContext'
 import { ErrorPage } from 'pages/Error/ErrorPage'
-import { Dataset } from '../../services/interfaces'
 import { SuccessNotification } from 'services/helpers/helpers'
 
 export function DatasetPage() {
   const [edit, setEdit] = useState<boolean>(false)
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [error, setError] = useState<any>()
-  const [dataset, setDataset] = useState<Dataset>({
-    fetal_fraction_preface: 0,
-    fetal_fraction_y_for_trisomy: 0,
-    fetal_fraction_y_max: 0,
-    fetal_fraction_y_min: 0,
-    fetal_fraction_XXX: 0,
-    fetal_fraction_X0: 0,
-    y_axis_min: 0,
-    y_axis_max: 0,
-    k_upper: 0,
-    m_lower: 0,
-    k_lower: 0,
-    m_upper: 0,
-    trisomy_soft_max: 0,
-    trisomy_hard_max: 0,
-    trisomy_hard_min: 0,
-    comment: '',
-  })
+  const [dataset, setDataset] = useState<any>()
   const { pathname } = useLocation()
   const userContext = useContext(UserContext)
   const datasetName = pathname.substring(pathname.lastIndexOf('/') + 1, pathname.length)
@@ -42,24 +24,7 @@ export function DatasetPage() {
   const getDatasetData = () => {
     getDataset(userContext, datasetName)
       .then((dataset) => {
-        setDataset({
-          fetal_fraction_y_max: dataset?.fetal_fraction_y_max,
-          fetal_fraction_preface: dataset?.fetal_fraction_preface,
-          trisomy_hard_min: dataset?.trisomy_hard_min,
-          y_axis_max: dataset?.y_axis_max,
-          k_lower: dataset?.k_lower,
-          fetal_fraction_y_min: dataset?.fetal_fraction_y_min,
-          k_upper: dataset?.k_upper,
-          m_lower: dataset?.m_lower,
-          m_upper: dataset?.m_upper,
-          fetal_fraction_X0: dataset?.fetal_fraction_X0,
-          fetal_fraction_y_for_trisomy: dataset?.fetal_fraction_y_for_trisomy,
-          fetal_fraction_XXX: dataset?.fetal_fraction_XXX,
-          y_axis_min: dataset?.y_axis_min,
-          trisomy_soft_max: dataset?.trisomy_soft_max,
-          trisomy_hard_max: dataset?.trisomy_hard_max,
-          comment: dataset?.comment,
-        })
+        setDataset(dataset)
         setIsLoading(false)
       })
       .catch((error) => {
