591aa569ec0cdcb34200bb080e756db57e38b065
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-05-23 10:47:34 +0200

    Check the edit save button visibility

--- src/components/DatasetPage/DatasetPage.test.tsx
+++ src/components/DatasetPage/DatasetPage.test.tsx
@@ -5,6 +5,7 @@ import { mockDatasets } from 'mocks/datasets'
 import { MemoryRouter } from 'react-router-dom'
 import { UserContext } from 'services/userContext'
 import axios from 'axios'
+import userEvent from '@testing-library/user-event'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -17,6 +18,7 @@ jest.mock('react-router-dom', () => ({
 
 describe('Dataset page', () => {
   test('Edit button should render for RW user', async () => {
+    const user = userEvent.setup()
     mockedAxios.get.mockReturnValue(
       Promise.resolve({
         data: {
@@ -46,9 +48,12 @@ describe('Dataset page', () => {
       )
     )
     const editBtn = await waitFor(() => queryAllByText(/Edit/i))
-    await waitFor(() => expect(editBtn).toHaveLength(1))
+    await waitFor(() => expect(editBtn[0]).toBeVisible())
     const saveBtn = await waitFor(() => queryAllByText(/Save/i))
-    await waitFor(() => expect(saveBtn).toHaveLength(1))
+    await waitFor(() => expect(saveBtn[0]).not.toBeVisible())
+    await user.click(editBtn[0])
+    await waitFor(() => expect(saveBtn[0]).toBeVisible())
+    await waitFor(() => expect(editBtn[0]).not.toBeVisible())
   })
   test('Edit button should not render for read user', async () => {
     mockedAxios.get.mockReturnValue(
