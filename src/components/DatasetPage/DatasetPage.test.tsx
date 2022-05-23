be0353a3e22250ff311aad8e8487a64ff1c125e8
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2022-05-23 13:08:53 +0200

    Add a backend call to the test

--- src/components/DatasetPage/DatasetPage.test.tsx
+++ src/components/DatasetPage/DatasetPage.test.tsx
@@ -21,9 +21,7 @@ describe('Dataset page', () => {
     const user = userEvent.setup()
     mockedAxios.get.mockReturnValue(
       Promise.resolve({
-        data: {
-          dataset: mockDatasets[0],
-        },
+        data: mockDatasets[0],
       })
     )
     const initializeUserContext = () => null
@@ -54,13 +52,53 @@ describe('Dataset page', () => {
     await user.click(editBtn[0])
     await waitFor(() => expect(saveBtn[0]).toBeVisible())
     await waitFor(() => expect(editBtn[0]).not.toBeVisible())
+    await user.click(saveBtn[0])
+  })
+  test('RW user should be able to edit dataset ', async () => {
+    const user = userEvent.setup()
+    mockedAxios.get.mockReturnValue(
+      Promise.resolve({
+        data: mockDatasets[0],
+      })
+    )
+    const initializeUserContext = () => null
+    const logout = () => null
+
+    const { queryAllByText } = await waitFor(() =>
+      render(
+        <UserContext.Provider
+          value={{
+            initializeUserContext,
+            logout,
+            token: 'token',
+            username: 'elevu',
+            email: 'testemail',
+            permissions: ['RW'],
+          }}
+        >
+          <MemoryRouter>
+            <DatasetPage />
+          </MemoryRouter>
+        </UserContext.Provider>
+      )
+    )
+    const saveBtn = await waitFor(() => queryAllByText(/Save/i))
+    await user.click(saveBtn[0])
+    expect(axios.patch).toHaveBeenLastCalledWith(
+      `undefined/dataset/${mockDatasets[0].name}`,
+      `trisomy_hard_min=${mockDatasets[0].trisomy_hard_min}&trisomy_hard_max=${mockDatasets[0].trisomy_hard_max}&trisomy_soft_max=${mockDatasets[0].trisomy_soft_max}&m_upper=6.5958&m_lower=${mockDatasets[0].m_lower}&k_lower=${mockDatasets[0].k_lower}&k_upper=${mockDatasets[0].k_upper}&y_axis_max=${mockDatasets[0].y_axis_max}&y_axis_min=${mockDatasets[0].y_axis_min}&fetal_fraction_X0=${mockDatasets[0].fetal_fraction_X0}&fetal_fraction_XXX=${mockDatasets[0].fetal_fraction_XXX}&fetal_fraction_y_min=${mockDatasets[0].fetal_fraction_y_min}&fetal_fraction_y_max=${mockDatasets[0].fetal_fraction_y_max}&fetal_fraction_y_for_trisomy=${mockDatasets[0].fetal_fraction_y_for_trisomy}&fetal_fraction_preface=${mockDatasets[0].fetal_fraction_preface}&comment=${mockDatasets[0].comment}`,
+      {
+        headers: {
+          Authorization: 'Bearer token',
+          ContentType: 'application/x-www-form-urlencoded',
+        },
+      }
+    )
   })
   test('Edit button should not render for read user', async () => {
     mockedAxios.get.mockReturnValue(
       Promise.resolve({
-        data: {
-          dataset: mockDatasets[0],
-        },
+        data: mockDatasets[0],
       })
     )
     const initializeUserContext = () => null
