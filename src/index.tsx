5ee689f08b25f17d6c359561138d41ca45186ad9
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-01-07 15:10:16 +0100

    Remove CRA serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
-import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import { setTwoToneColor } from '@ant-design/icons'
@@ -37,4 +36,3 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
