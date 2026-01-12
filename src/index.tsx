0b3bd0bb84c1194c555a6b06d068d1c535278c23
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-01-12 15:50:13 +0100

    Remove CRA service worker comment and drop PWA manifest remnants

--- src/index.tsx
+++ src/index.tsx
@@ -35,7 +35,3 @@ root.render(
     </BrowserRouter>
   </ConfigProvider>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
