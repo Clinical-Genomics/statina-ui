af436498b12d7fbad986662649b6d4f156fefc89
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-01-13 15:34:43 +0100

    Add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'node:path'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      'buffer/': 'buffer',
-      'plotly.js/dist/plotly': path.resolve(
-        __dirname,
-        'node_modules/plotly.js/dist/plotly-basic.min.js'
-      ),
-      services: path.resolve(__dirname, 'src/services'),
-      components: path.resolve(__dirname, 'src/components'),
-      pages: path.resolve(__dirname, 'src/pages'),
-      assets: path.resolve(__dirname, 'src/assets'),
-    },
-  },
   server: {
     port: 3000,
   },
